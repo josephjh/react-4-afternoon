316ad0a37bcf2bc0d240d6ebdade48f271efe461
Joseph Hamblin <joseph.jay.hamblin@gmail.com>
2019-02-12 16:27:50 -0700

    completed project

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
+import {Link, Switch, Route} from 'react-router-dom';
+import History from '../History/History';
+import Contact from '../Contact/Contact';
 
 export default class About extends Component {
   render() {
     return (
       <div>
         <div className='subnav'>
+          <Link to="/about" className="subnav_links"><h3>About</h3></Link>
+          <Link to="/about/history" className="subnav_links"><h3>History</h3></Link>
+          <Link to="/about/contact" className="subnav_links"><h3>Contact</h3></Link>
         </div>
         <div className='box'>
+          <Switch>
+            <Route path="/about/history"/>
+            <Route path="/about/contact"/>
+            <Route path="/" render={() => (
+              <div>
+                <h1>About the University</h1>
+                <p>"At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat."</p>
+              </div>
+            )}/>
+          </Switch>
         </div>
       </div>
     )
